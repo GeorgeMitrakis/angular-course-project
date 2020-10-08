34a4cadbe4503d4ca9190a4dc39c24c70c697069
GeorgeMitrakis <georgemitr96@hotmail.com>
2020-10-08 12:35:53 +0300

    added ingredient array to recipe model

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -6,9 +7,23 @@ import { Recipe } from './recipe.model';
 })
 export class RecipesService {
   recipes: Recipe[] = [
-    new Recipe('Angular', 'Google\'s Js Framework', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png'),
-    new Recipe('React', 'Facebook\'s Js Library', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png'),
-    new Recipe('Vue', 'The Community\'s Js Framework', 'https://vuejs.org/images/logo.png')
+    new Recipe(
+      'Tasty Schnitzel', 
+      'A super-tasty Schnitzel - just awesome!', 
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+
+    new Recipe(
+      'Big Fat Burger', 
+      'What else you need to say?', 
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ];
 
   recipeWasSelected = new EventEmitter<Recipe>();
