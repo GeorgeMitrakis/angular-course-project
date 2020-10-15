a5982163cca2f54d2e3b1edfe396a1348465ce30
GeorgeMitrakis <georgemitr96@hotmail.com>
2020-10-15 07:11:08 +0300

    delete ingredients on recipe edit (#237)

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -88,6 +88,10 @@ export class RecipeEditComponent implements OnInit {
     // )
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index)
+  }
+
   onSubmit(){
     // console.log(this.recipeForm)
     const submittedRecipe = new Recipe(
