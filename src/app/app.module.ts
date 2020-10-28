615043da0a00b11f260fd34a79ed089aab8babdb
GeorgeMitrakis <georgemitr96@hotmail.com>
2020-10-28 16:49:56 +0200

    the core module (#327)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { HeaderComponent } from './header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptor } from './auth/auth.interceptor';
 import { RecipesModule } from './recipes/recipes.module';
 import { ErrorPageModule } from './error-page/error-page.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,17 +25,11 @@ import { SharedModule } from './shared/shared.module';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    CoreModule,
     SharedModule,
     RecipesModule,
     ShoppingListModule,
-    ErrorPageModule,  // order matters, because of the wildcard routes !!!
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi:true
-    }
+    ErrorPageModule  // order matters, because of the wildcard routes !!!
   ],
   bootstrap: [AppComponent]
 })
