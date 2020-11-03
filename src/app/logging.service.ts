d3e89019594581856c5a032e7c4b077e3b93bd17
GeorgeMitrakis <georgemitr96@hotmail.com>
2020-11-04 01:15:57 +0200

    added conditional logging upon production

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggingService {
 
-  constructor() {console.log(`%c LoggingService.constructor()`, `color: cornflowerblue; font-weight: bold;`)}
+  constructor() {
+    if(environment.production){
+      return;
+    }
+    console.log(`%c LoggingService.constructor()`, `color: cornflowerblue; font-weight: bold;`)
+  }
 
   log(message: string, color: string, fontWeight?:string){
+
+    if(environment.production){
+      return;
+    }
+    
     if(!fontWeight){
       fontWeight = 'normal';
     }
